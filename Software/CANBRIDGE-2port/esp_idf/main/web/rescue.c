51e1f005aaa030b1e9b9fa6fec8b208b683ac537
furdog <138627600+furdog@users.noreply.github.com>
2026-02-13 17:06:16 +0200

    update: WIP

--- Software/CANBRIDGE-2port/esp_idf/main/web/rescue.c
+++ Software/CANBRIDGE-2port/esp_idf/main/web/rescue.c
@@ -16,8 +16,6 @@
 /* Backtrace */
 #include "esp_debug_helpers.h"
 
-#include "dns_tools.h"
-
 static const char *TAG = "RESCUE_SERVER";
 static httpd_handle_t server = NULL;
 
@@ -71,67 +69,6 @@ int ws_logger_hook(const char *fmt, va_list tag) {
 }
 
 /*****************************************************************************
- * DNS
- *****************************************************************************/
-/** This task redirects all incoming DNS requests to 7.7.7.7 */
-void dns_server_task(void *pvParameters) {
-	uint8_t data[256]; // Increased size for safety
-
-	struct sockaddr_in dest_addr = {
-		.sin_addr.s_addr = htonl(INADDR_ANY),
-		.sin_family = AF_INET,
-		.sin_port = htons(53)
-	};
-
-	int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
-	bind(sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
-
-	ESP_LOGI(TAG, "DNS Sniffer Active.");
-
-	while (1) {
-		struct sockaddr_in source_addr;
-		socklen_t addr_len = sizeof(source_addr);
-		int len = recvfrom(sock, data, sizeof(data), 0,
-				   (struct sockaddr *)&source_addr, &addr_len);
-
-		struct dns_msg msg;
-		dns_msg_init(&msg, data, sizeof(data));
-		dns_msg_parse_query(&msg, len);
-
-		uint8_t answer[] = {
-			0xc0, 0x0c,             // Pointer to name
-			0x00, 0x01,             // Type A (Force IPv4)
-			0x00, 0x01,             // Class IN
-			0x00, 0x00, 0x00, 0x0a, // TTL 10s
-			0x00, 0x04,             // Data Len 4
-			7, 7, 7, 7              // IP 7.7.7.7
-		};
-
-		/* This answer redirects every request to 7.7.7.7 */
-		size_t total_len = dns_msg_add_answer(&msg, answer,
-							  sizeof(answer));
-
-		sendto(sock, data, total_len, 0,
-			   (struct sockaddr *)&source_addr, addr_len);
-
-		if (msg.malformed == 0u) {
-			uint32_t ip = source_addr.sin_addr.s_addr;
-
-			ESP_LOGI("DNS", "Query from %u.%u.%u.%u "
-				 "| Type: %s | Domain: %s",
-				 (unsigned int)(ip & 0xFF),
-				 (unsigned int)((ip >> 8) & 0xFF),
-				 (unsigned int)((ip >> 16) & 0xFF),
-				 (unsigned int)((ip >> 24) & 0xFF),
-				 dns_msg_get_type_str(&msg), msg.name);
-		} else {
-			ESP_LOGI("DNS", "Query is malformed dns_tools.h, line: %u",
-				 msg.malformed);
-		}
-	}
-}
-
-/*****************************************************************************
  * HTTP stuff
  *****************************************************************************/
 // --- 3. HTTP Handlers ---
@@ -155,27 +92,6 @@ static esp_err_t captive_portal_redirect_handler(httpd_req_t *req) {
 static esp_err_t index_get_handler(httpd_req_t *req) {
 	httpd_resp_set_type(req, "text/html");
 	return httpd_resp_send(req, (const char *)index_html_start, index_html_end - index_html_start);
-}
-
-static esp_err_t ws_handler(httpd_req_t *req) {
-	if (req->method == HTTP_GET) {
-		int websocket_fd = httpd_req_to_sockfd(req);
-		ws_server_client_add(&ws_server, websocket_fd);
-
-		return ESP_OK;
-	}
-
-	httpd_ws_frame_t ws_pkt;
-	memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
-    
-	// If this returns anything other than ESP_OK, the server kills the session
-	esp_err_t ret = httpd_ws_recv_frame(req, &ws_pkt, 0);
-	if (ret != ESP_OK) {
-		ESP_LOGE(TAG, "WS recv failed, killing session");
-		return ESP_FAIL; // <--- THIS is what triggers the close_fn
-	}
-
-	return ESP_OK;
 }
 
 // --- 4. Wi-Fi & System Init ---
@@ -217,68 +133,6 @@ static esp_err_t qrcode_js_handler(httpd_req_t *req) {
 	return httpd_resp_send(req, (const char *)qrcode_js_start, qrcode_js_end - qrcode_js_start);
 }
 
-/** Override default recv with detailed one, to debug errno 128 spam
- * DEBUG PURPOSES */
-static int httpd_sock_err(const char *ctx, int sockfd)
-{
-	int errval;
-	ESP_LOGW(TAG, "error in sock %s : %d", ctx, errno);
-
-	switch (errno) {
-	case EAGAIN:
-	case EINTR:
-		errval = HTTPD_SOCK_ERR_TIMEOUT;
-		break;
-	case EINVAL:
-	case EBADF:
-	case EFAULT:
-	case ENOTSOCK:
-		errval = HTTPD_SOCK_ERR_INVALID;
-		break;
-	default:
-		errval = HTTPD_SOCK_ERR_FAIL;
-	}
-	return errval;
-}
-
-int httpd_detailed_recv(httpd_handle_t hd, int sockfd, char *buf, size_t buf_len, int flags)
-{
-	(void)hd;
-
-	if (buf == NULL) {
-		return HTTPD_SOCK_ERR_INVALID;
-	}
-
-	int ret = recv(sockfd, buf, buf_len, flags);
-	if (ret < 0) {
-		/* Default logger */
-		//esp_log_set_vprintf(vprintf);
-
-		if (errno == 128) {
-			//ESP_LOGW(TAG, "Caught ENOTCONN. Backtrace:");
-			//esp_backtrace_print(10); // Print the last 10 stack frames
-			//((void (*)(void))0)();
-		}
-
-		ESP_LOGW(TAG, "fd: %d (WS=%s)" , sockfd,
-			httpd_ws_get_fd_info(hd, sockfd) ==
-				HTTPD_WS_CLIENT_WEBSOCKET ? "YES" : "NO"); /* Print info about file descriptor */
-
-		/* Default logger */
-		//esp_log_set_vprintf(ws_logger_hook);
-
-		return httpd_sock_err("recv", sockfd);
-	}
-
-	return ret;
-}
-
-/** Custom open handler */
-esp_err_t my_open_func(httpd_handle_t hd, int sockfd) {
-	ESP_LOGI(TAG, "New session opened (fd: %d). Overriding recv.", sockfd);
-	return httpd_sess_set_recv_override(hd, sockfd, httpd_detailed_recv);
-}
-
 /** Must be called after websocket connection is closed
  *  can be passed as close_fn in server config */
 void ws_client_del_thread_safe(httpd_handle_t hd, int fd)
@@ -289,6 +143,9 @@ void ws_client_del_thread_safe(httpd_handle_t hd, int fd)
 	// This is why Error 23 was happening.
 	close(fd);
 }
+
+extern esp_err_t update_options_handler(httpd_req_t *req);
+extern esp_err_t update_post_handler(httpd_req_t *req);
 
 static void start_webserver(void) {
 	httpd_config_t config = HTTPD_DEFAULT_CONFIG();
@@ -302,33 +159,38 @@ static void start_webserver(void) {
 	config.keep_alive_interval = 2;    // 2 seconds between probes
 	config.keep_alive_count    = 3;    // 3 failed probes = close socket
 
+	config.max_uri_handlers = 12;
+
 	/* Register websocket stuff */
-	config.open_fn = my_open_func;
 	config.close_fn = ws_client_del_thread_safe;
 
+	ws_server_init(&ws_server);
+
 	if (httpd_start(&server, &config) == ESP_OK) {
-		httpd_uri_t uris[6] = {
+		static httpd_uri_t uris[7] = {
 			{ .uri = "/", .method = HTTP_GET, .handler = index_get_handler },
 			{ .uri = "/qrcode.min.js", .method = HTTP_GET, .handler = qrcode_js_handler },
 			{ .uri = "/generate_204", .method = HTTP_GET, .handler = captive_portal_redirect_handler },
 			{ .uri = "/ncsi.txt", .method = HTTP_GET, .handler = captive_portal_redirect_handler },
 			{ .uri = "/redirect", .method = HTTP_GET, .handler = captive_portal_redirect_handler },
-			{ .uri = "/ws", .method = HTTP_GET, .handler = ws_handler, .is_websocket = true }
+			{ .uri = "/update", .method = HTTP_OPTIONS, .handler = update_options_handler },
+			{ .uri = "/update", .method = HTTP_POST, .handler = update_post_handler }
 		};
-		for (int i = 0; i < 6; i++) httpd_register_uri_handler(server, &uris[i]);
+		for (int i = 0; i < 7; i++) httpd_register_uri_handler(server, &uris[i]);
+
 		//httpd_register_err_handler(server, HTTPD_404_NOT_FOUND, captive_portal_redirect_handler);
 	}
 
-	ws_server_init(&ws_server);
 	ws_server_start(&ws_server, &server);
 }
 
 // The function you want to run every second
 void my_periodic_action() {
-	const char *text = "Hello websockets!"; 
-	ESP_LOGI(TAG, "(1) fd: %i, handle: %p, frame.len: %i",
-		59, server, strlen(text));
-	ws_server_broadcast_text(&ws_server, text, strlen(text));
+	char *text;
+	if (ws_server_peek_text_rx(&ws_server, &text) != 0) {
+		ESP_LOGI(TAG, "message arrived! %s", text);
+		ws_server_dequeue_text_rx(&ws_server, text);
+	}
 }
 
 void second_timer_task(void *pvParameters) {
@@ -347,6 +209,8 @@ void second_timer_task(void *pvParameters) {
 	}
 }
 
+void dns_server_task(void *pvParameters);
+
 void rescue_main(void) {
 	ESP_ERROR_CHECK(nvs_flash_init());
 	ESP_ERROR_CHECK(esp_netif_init());
@@ -358,7 +222,7 @@ void rescue_main(void) {
 
 	// Launch background tasks
 	xTaskCreate(dns_server_task,   "dns_server",   3072, NULL, 1, NULL);
-	//xTaskCreate(second_timer_task, "one_sec_timer", 2048, NULL, 5, NULL);
+	xTaskCreate(second_timer_task, "one_sec_timer", 2048, NULL, 5, NULL);
 
 	// Finally, redirect system logs to our ring buffer
 	esp_log_set_vprintf(ws_logger_hook);
